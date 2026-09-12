ed0e1f743918e80d8808229f17ff89a7522dddd1
git@github.com:svitlanasemeniuk/goit-js-hw-10.git <svitlana.semeniuk11@gmail>
2026-09-12 17:27:22 +0200

    fixing errors

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './components/App/App'
-import 'modern-normalize'
+import 'modern-normalize/modern-normalize.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
