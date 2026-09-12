adb0df5e6f8358aee3a724c9c34c8a29daac9650
git@github.com:svitlanasemeniuk/goit-js-hw-10.git <svitlana.semeniuk11@gmail>
2026-09-12 22:11:04 +0200

    fix api key

--- src/services/moviesService.ts
+++ src/services/moviesService.ts
@@ -1,19 +1,18 @@
 import axios from 'axios';
-import type { Movie } from '../types/movie'; 
+import type { Movie } from '../types/movie';
 
 axios.defaults.baseURL = 'https://api.themoviedb.org/3';
 
 export const fetchMovies = async (query: string): Promise<Movie[]> => {
-const response = await axios.get('/search/movie', {
-    params: {
-    query,
-    include_adult: false,
-    language: 'en-US',
-    page: 1,
-    },
-    headers: {
-    Authorization: `Bearer ${import.meta.env.VITE_TMDB_API_KEY}`,
-    },
-});
-return response.data.results;
+    const response = await axios.get('/search/movie', {
+        params: {
+            api_key: import.meta.env.VITE_TMDB_API_KEY, 
+            query,
+            include_adult: false,
+            language: 'en-US',
+            page: 1
+    }
+    });
+
+    return response.data.results;
 };
